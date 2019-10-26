687ce9f0385208a92d6f528752f6080209422208
XWQ2018 <18476969365@163.com>
2019-10-26 11:34:33 +0800

    配置修改

--- app.vue
+++ app.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="page">
-        <h1>单文件组件</h1>
-        <img src="./public/img/dialogIcon.png" alt="dialog">
+        <h1>单文件组件111</h1>
+        <img src="./public/img/dialogIcon.png" alt="dialog" />
     </div>
 </template>
 <script>
+import "amfe-flexible/index.js";
 export default {
     data() {
         return {};
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
+const pxtorem = require('postcss-pxtorem');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
     publicPath: './',
-
-
 
     css: {
         // 是否使用css分离插件 ExtractTextPlugin Default: 生产环境下是 true，开发环境下是 false
@@ -10,16 +11,18 @@ module.exports = {
         sourceMap: false,
         // 启用 CSS modules for all css / pre-processor files.
         modules: false,
+
         loaderOptions: {
-            /*   postcss: {
-                  plugins: [
-                      require('postcss-pxtorem')({
-                          rootValue: 37.5, // 换算的基数
-                          // selectorBlackList: ['weui', 'mu'], // 忽略转换正则匹配项
-                          propList: ['*'],
-                      })
-                  ]
-              }, */
+            postcss: {
+                plugins: [
+                    pxtorem({
+                        rootValue: 37.5, // 换算的基数
+                        // selectorBlackList: ['weui', 'mu'], // 忽略转换正则匹配项
+                        propList: ['*'],
+                    }),
+                    autoprefixer({ broswer: 'last 5 versions' }) //CSS3前缀添加s
+                ]
+            },
             less: {
                 test: /\.less$/,
                 use: [{
